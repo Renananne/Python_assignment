8749ed92a2c80012e9544a957ff6dede818101c9
Renananne <156876832+Renananne@users.noreply.github.com>
2024-02-21 13:25:50 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your locatipn: ")
+print("Hello " + name , end=",")
+print("you are " +age, end=" ")
+print("years old and live in " + location)
